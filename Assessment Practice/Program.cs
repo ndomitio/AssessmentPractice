f4350335a1d3d2b9ef56b1289d822cc42231beec
ndomitio <nicholas.domitio@gmail.com>
2018-05-21 14:07:41 -0400

    first commit

---
+++ Assessment Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_Practice
+{
+    class Program
+    {
+        for (int i = 0; i<n; i++)
+			{
+
+			}
+}
+       
+           
+    
+       
